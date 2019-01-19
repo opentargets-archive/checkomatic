ed7d98bf7e1f598aef546e5a7400707b9337cc20
Miguel C <mkarmona@gmail.com>
2019-01-19 10:47:38 +0000

    wip: implement a proper iterable through all subsections in each k

--- checkomatic/cli.py
+++ checkomatic/cli.py
@@ -19,9 +19,15 @@ def eval_section(assertion_list, global_dict, local_env):
     def _eval_elem(elem, global_scope, local_scope):
         if '\n' in elem:
             exec(elem, global_scope, local_scope)
-            return local_scope['output']
+            r = local_scope['output']
+            if not r:
+                print("failed at", elem)
+            return r
         else:
-            return eval(elem, global_scope, local_scope)
+            r = eval(elem, global_scope, local_scope)
+            if not r:
+                print("failed at", elem)
+            return r
 
     return list(iters.imap(lambda e: _eval_elem(e, global_dict, local_env), assertion_list))
 
@@ -42,18 +48,39 @@ def eval_stats(ot_client, assertion_list, global_env):
 
 
 def eval_targets(ot_client, section_dict, global_env):
-    for i, (k, v) in enumerate(section_dict.items(), start=1):
-        data = ot_client.search(k, size=1)
+    asserted = []
+    for ((k, v), i) in fn.seq(section_dict.items()).zip_with_index(start=1).to_list():
+        data = ot_client.search(k, size=1, filter="target")
         if data.total > 0:
             local_env = make_local_env(input=None, output=False)
             target_o = addict.Dict(fn.seq(data.to_object()).take(1).head().data)
             local_env['o'] = target_o
-            asserted = eval_section(v, global_env, local_env)
-            print("eval target", i, k, "asserted", str(asserted))
-            return asserted
+            asserted_k = eval_section(v, global_env, local_env)
+            print("eval target", i, k, "asserted", str(asserted_k))
+            asserted += asserted_k
         else:
             print("eval target", i, k, "asserted", None)
-            return []
+            asserted += [False]
+
+    return asserted
+
+
+def eval_diseases(ot_client, section_dict, global_env):
+    asserted = []
+    for ((k, v), i) in fn.seq(section_dict.items()).zip_with_index(start=1).to_list():
+        data = ot_client.search(k, size=1, filter="disease")
+        if data.total > 0:
+            local_env = make_local_env(input=None, output=False)
+            disease_o = addict.Dict(fn.seq(data.to_object()).take(1).head().data)
+            local_env['o'] = disease_o
+            asserted_k = eval_section(v, global_env, local_env)
+            print("eval disease", i, k, "asserted", str(asserted_k))
+            asserted += asserted_k
+        else:
+            print("eval disease", i, k, "asserted", None)
+            asserted += [False]
+
+    return asserted
 
 
 def main(filename):
@@ -66,16 +93,19 @@ def main(filename):
         results = []
         if c.checkomatic.rules.targets:
             results += eval_targets(ot_client, c.checkomatic.rules.targets.to_dict(), _g)
+        if c.checkomatic.rules.diseases:
+            results += eval_diseases(ot_client, c.checkomatic.rules.diseases.to_dict(), _g)
         if c.checkomatic.rules.stats:
             results += eval_stats(ot_client, c.checkomatic.rules.stats, _g)
 
         oks = fn.seq(results).filter(lambda e: e == True).count_by_value()
         fails = fn.seq(results).filter(lambda e: e == False).count_by_value()
-        print("result", results, "fails", fails, "oks", oks)
+        print("oks", oks, "fails", fails)
 
         if fails > 0 or oks == 0:
-            raise RuntimeError("oks %s fails %s" % (oks, fails))
-        return 0
+            return 1
+        else:
+            return 0
 
 
 if __name__ == '__main__':
